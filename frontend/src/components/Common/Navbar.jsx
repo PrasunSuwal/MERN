058b765e17fc49e4dcd8b812f81136590cb29d60
Prasun Suwal <prasun.suwal@gmail.com>
2025-04-24 23:02:44 +0545

    route

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -12,14 +12,14 @@ import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const [navDrawOpen, setNavDrawOpen] =useState(false);
+  const [navDrawOpen, setNavDrawOpen] = useState(false);
 
   const toggleNavDrawer = () => {
     setNavDrawOpen(!navDrawOpen);
-  }
+  };
 
   const toggleCartDrawer = () => {
-    setDrawerOpen(prev => !prev);
+    setDrawerOpen((prev) => !prev);
   };
 
   return (
@@ -34,7 +34,7 @@ const Navbar = () => {
         {/* {Center - Nav LInks} */}
         <div className="hidden md:flex space-x-6">
           <Link
-            to="#"
+            to="/collections/all"
             className="text-gray-700 hover:text-black text-sm font-medium uppercase"
           >
             Men
@@ -83,25 +83,52 @@ const Navbar = () => {
         </div>
       </nav>
       <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
-      
-      {/* mobile navigation */}
-      <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawOpen ? "translate-x-0" : "-translate-x-full"} `}>
-      <div className="flex justify-end p-4">
-        <button onClick={toggleNavDrawer}>
-            <IoMdClose className="h-6 w-6 text-gray-600"/>
-        </button>
-      </div>
-      <div className="p-4">
-        <h2 className="text-xl font-semibold mb-4">
-            <nav className="space-y-4">
-                <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Men</Link>
-                <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Women</Link>
-                <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Top Wear</Link>
-                <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Bottom Wear</Link>
-            </nav>
 
-        </h2>
-      </div>
+      {/* mobile navigation */}
+      <div
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+          navDrawOpen ? "translate-x-0" : "-translate-x-full"
+        } `}
+      >
+        <div className="flex justify-end p-4">
+          <button onClick={toggleNavDrawer}>
+            <IoMdClose className="h-6 w-6 text-gray-600" />
+          </button>
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">
+            <nav className="space-y-4">
+              <Link
+                to="#"
+                onClick={toggleNavDrawer}
+                className="block text-gray-600 hover:text-black"
+              >
+                Men
+              </Link>
+              <Link
+                to="#"
+                onClick={toggleNavDrawer}
+                className="block text-gray-600 hover:text-black"
+              >
+                Women
+              </Link>
+              <Link
+                to="#"
+                onClick={toggleNavDrawer}
+                className="block text-gray-600 hover:text-black"
+              >
+                Top Wear
+              </Link>
+              <Link
+                to="#"
+                onClick={toggleNavDrawer}
+                className="block text-gray-600 hover:text-black"
+              >
+                Bottom Wear
+              </Link>
+            </nav>
+          </h2>
+        </div>
       </div>
     </>
   );
