e01ad8031f79a3096bb7b3101f3faf73e2ad8f41
Prasun Suwal <prasun.suwal@gmail.com>
2025-04-26 23:14:17 +0545

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,10 @@ import Profile from './pages/Profile';
 import CollectionPage from './pages/CollectionPage';
 import ProductDetails from './components/Products/ProductDetails';
 import Checkout from './components/Cart/Checkout';
+import OrderConfirmationPage from './pages/OrderConfirmationPage';
+import OrderDetailsPage from './pages/OrderDetailsPage';
+import MyOrders from './pages/MyOrders';
+import AdminLayout from './components/Admin/AdminLayout';
 
 const App = () => {
   return (
@@ -23,8 +27,12 @@ const App = () => {
         <Route path="collections/:collection" element={<CollectionPage/>}/>
         <Route path="product/:id" element={<ProductDetails/>}/>
         <Route path="checkout" element={<Checkout/>}/>
+        <Route path="order-confirmation" element={<OrderConfirmationPage/>}/>
+        <Route path="order/:id" element={<OrderDetailsPage/>}/>
+        <Route path="/my-orders" element={<MyOrders/>}/> 
       </Route>
-      <Route>{/*Admin Layout*/}</Route>
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminLayout/>}></Route>
     </Routes>
     </BrowserRouter>
   )
