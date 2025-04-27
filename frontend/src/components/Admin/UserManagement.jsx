a6e4f18f5aef349996a0cc8a8d91981f0dbbcc89
Prasun Suwal <prasun.suwal@gmail.com>
2025-04-27 23:06:31 +0545

    user management

---
+++ frontend/src/components/Admin/UserManagement.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+import { useState } from "react";
+
+const UserManagement = () => {
+  const users = [
+    {
+      _id: "1",
+      name: "John Doe",
+      email: "john@example.com",
+      role: "admin",
+    },
+  ];
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: "customer", //default role
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      name: "",
+      email: "",
+      password: "",
+      role: "customer",
+    });
+  };
+
+  const handleRoleChange = (userId, newRole) => {
+    // Update the role of the user with userId to newRole
+    // This is a placeholder function. In a real application, you would send a request to your backend to update the user's role.
+    console.log(`User ID: ${userId}, New Role: ${newRole}`);
+  };
+
+  const handleDeleteUser = (userId) => {
+    // Delete the user with userId
+    if (window.confirm("Are you sure you want to delete this user?")) {
+        console.log(`User with ID: ${userId} deleted`);
+    }
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto p-6 ">
+      <h2 className="text-2xl md:text-3xl font-bold mb-6">User Management</h2>
+      {/* Add new user Form */}
+      <div className="p-6 rounded-lg ">
+        <h3 className="text-lg font-bold mb-4">Add New User</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="role"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Role
+            </label>
+            <select
+              id="role"
+              name="role"
+              value={formData.role}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+            >
+              <option value="customer">Customer</option>
+              <option value="admin">Admin</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          >
+            Add User
+          </button>
+        </form>
+      </div>
+
+      {/* User list management */}
+      <div className="overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="min-w-full text-left text-gray-500">
+          <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+            <tr>
+              <th className="py-3 px-4">Name</th>
+              <th className="py-3 px-4">Email</th>
+              <th className="py-3 px-4">Role</th>
+              <th className="py-3 px-4">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id} className="border-b hover:bg-gray-50">
+                <td className="p-4 font-medium text-gray-900 whitespace-nowrap">
+                  {user.name}
+                </td>
+                <td className="p4">{user.email}</td>
+                <td className="p4">
+                  <select
+                    value={user.role}
+                    onChange={(e) => handleRoleChange(user._id, e.target.value)}
+                    className="p-2 border rounded"
+                  >
+                    <option value="customer">Customer</option>
+                    <option value="admin">Admin</option>
+                  </select>
+                </td>
+                <td className="py-3 px-4">
+                  <button
+                    onClick={() => handleDeleteUser(user._id)}
+                    className="bg-red-500 text-white px-4 py-2 rounded hover:text-red-700"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UserManagement;
