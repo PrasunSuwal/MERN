c3d0bbb690b86f31c98313abbd3b7fd196820744
Prasun Suwal <prasun.suwal@gmail.com>
2025-05-03 22:04:37 +0545

    server setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
+const userRoutes = require('./routes/userRoutes.js');
+const productRoutes = require('./routes/productRoutes.js');
 
 const app = express();
 app.use(express.json());
@@ -18,6 +20,11 @@ connectDB();
 app.get("/", (req, res) => {
     res.send("Hello from the server!");
 });
+
+//API routes
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
